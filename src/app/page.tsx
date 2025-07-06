b54fe9f25d9d690ad3ce368e6ae07269f9761667
aydengidley <gidleya1015@gmail.com>
2025-07-06 16:38:36 -0500

    Centered landing page text and minor tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-gray-950 text-white p-10">
-      <h1 className="text-4xl font-bold">KAIROS</h1>
-      <p className="text-lg mt-2">"The Critical Moment. Streamlining coordination and effectively visualing key data."</p>
+      <h1 className="text-4xl font-bold flex justify-center">KAIROS</h1>
+      <p className="text-lg mt-5 flex justify-center">"The Critical Moment"</p>
+      <p className="text-lg flex justify-center">Extending and streamlining the capabalities of external church service</p>
     </main>
   );
 }
